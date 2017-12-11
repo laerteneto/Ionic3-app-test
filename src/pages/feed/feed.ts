abb17402d03ec59d7ecc9d008859e5c5e4fe2df4
Laerte Neto <laerte.mneto@gmail.com>
2017-12-11 14:56:52 -0300

    colocando loading na pagina de feed de filmes

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { MoovieProvider } from '../../providers/moovie/moovie';
 
 /**
@@ -32,28 +32,72 @@ export class FeedPage {
 
 
   public nome_usuario:string = "Laerte Neto do Código";
+  public loader;
+  public refresher;
+  public isRefreshing: boolean = false;
+
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    private moovieProvider: MoovieProvider
-  ){
+    private moovieProvider: MoovieProvider,
+    public loadingCtrl: LoadingController
+  ){}
+
+  openLoading() {
+    this.loader = this.loadingCtrl.create({
+      content: "Carregando filmes..."
+    });
+    this.loader.present();
+  }
+
+  closeLoading() {
+   this.loader.dismiss(); 
   }
 
   public somaDoisNumeros(num1:number,num2:number): void{
     //alert(num1+num2);
   }
 
-  ionViewDidLoad() {
+  doRefresh(refresher) {
+    this.refresher = refresher;
+    this.isRefreshing = true;
+    this.carregarFilmes();
+  }
+
+  ionViewDidEnter() {
+    this.carregarFilmes();
+  }
+
+  carregarFilmes(){
+    this.openLoading();
+    
     this.moovieProvider.getLatestMovies().subscribe(
       data => {
         const response = (data as any);
         const obg_return = JSON.parse(response._body);
         this.movie_list = obg_return.results;
         console.log(obg_return);
+        
+        this.closeLoading();
+        
+        if (this.isRefreshing){
+          this.refresher.complete();
+          this.isRefreshing = false;
+        }
+
       },
+
       error =>{
         console.log(error);
+        
+        this.closeLoading();
+        
+        if (this.isRefreshing){
+          this.refresher.complete();
+          this.isRefreshing = false;
+        }
+      
       }
     )
   }
