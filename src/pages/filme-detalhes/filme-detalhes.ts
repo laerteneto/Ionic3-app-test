0a540376ff49d83d5ac9aa4054ae8d4b69beb072
Laerte Neto <laerte.mneto@gmail.com>
2017-12-12 12:37:48 -0300

    testando camera

--- src/pages/filme-detalhes/filme-detalhes.ts
+++ src/pages/filme-detalhes/filme-detalhes.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { MoovieProvider } from '../../providers/moovie/moovie';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 /**
  * Generated class for the FilmeDetalhesPage page.
@@ -13,28 +14,51 @@ import { MoovieProvider } from '../../providers/moovie/moovie';
 @Component({
   selector: 'page-filme-detalhes',
   templateUrl: 'filme-detalhes.html',
-  providers: [MoovieProvider]
+  providers: [
+    MoovieProvider,
+    Camera
+  ]
 })
 export class FilmeDetalhesPage {
 
   public movie;
   public movie_id;
 
+  public img="";
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public movieProvider: MoovieProvider
+    public movieProvider: MoovieProvider,
+    private camera: Camera
   ) { }
 
   ionViewDidEnter() {
     this.movie_id = this.navParams.get("id");
     this.movieProvider.getMovieDetails(this.movie_id).subscribe(
-      data=>{
+      data => {
         let retorno = (data as any)._body;
         this.movie = JSON.parse(retorno);
       }, error => {
         console.log(error);
       });
+  }
+
+  tirarFoto() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      this.img = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+    });
   }
 
 }
